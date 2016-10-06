531ee0ecbd160f9e5b9d34dc9e301a050a62f1e4
ASUS\Pedro <ordep07@hotmail.com>
2016-10-06 19:45:16 +0200

    Intento 3 de arreglo del Model Util

---
+++ ModelUtil/Exceptions/SqlException.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Common;
+
+namespace Es.Udc.DotNet.ModelUtil.Exceptions
+{
+
+    public class SqlException : DbException
+    {
+
+        public SqlException(string msg)
+            : base(msg) { }
+
+    }
+
+}
