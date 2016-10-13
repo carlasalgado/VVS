0908c2d86af818dff6b1761c688250c05be4ca09
ASUS\Pedro <ordep07@hotmail.com>
2016-10-13 18:56:26 +0200

    Test NUnit

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+
+namespace Test
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);     
+        }
+    }
+}
---
+++ TestNUnit/NUnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestNUnit
+{
+    [TestFixture]
+    public class NUnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
